8b59d2bda975d20316443175639395086fc6ef22
dOkI <denis.kirpichenkov@gmail.com>
2015-07-26 00:07:28 +0500

    HotSpot internal debugger start script

---
+++ HSDB_7.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+JAVA_HOME=/usr/lib/jvm/java-7-oracle
+
+if [ -z "$1" ]; then
+    echo "pass app PID as first parameter"
+    exit 1
+fi
+
+echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+
+java -cp ${JAVA_HOME}/lib/tools.jar:${JAVA_HOME}/lib/sa-jdi.jar:jdi-sa-heap-walk.jar sun.jvm.hotspot.HSDB
